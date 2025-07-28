51cd6033ca0a7808a6369c92ed05a3c19cf85bb5
excellence-man <excellence_man@tutamail.com>
2025-07-28 10:27:50 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// sw.js
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js');
+}
+
+// Nav
+  // Simple smooth scroll for nav links
+  document.querySelectorAll('nav a').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+          e.preventDefault();
+          const targetID = this.getAttribute('href').substring(1);
+          const target = document.getElementById(targetID);
+          if (target) {
+            window.scrollTo({
+              top: target.offsetTop - 60,
+              behavior: 'smooth'
+            });
+          }
+      });
+  });
+
+// Year
+document.addEventListener("DOMContentLoaded", function() {
+  const yearSpan = document.getElementById("currentYear");
+  if (yearSpan) yearSpan.textContent = new Date().getFullYear();
+});
